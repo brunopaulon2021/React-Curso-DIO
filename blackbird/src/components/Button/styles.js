65baaff5c50264294b0193375a76f0ced0ab3a4f
Bruno Paulon <paulonbruno9@gmail.com>
2022-12-08 14:17:16 +0000

    create project

---
+++ blackbird/src/components/Button/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+background: #565656;
+border-radius: 4px;
+color: #fff;
+position:relative;
+padding: 2px 12px;
+min-width: 120px;
+width:100%;
+
+${ ({variant}) => variant != "primary" && css `
+min-width:167px;
+height:33px
+bacground: #E41050;
+&::after { 
+    content: '';
+    position: absolute;
+    border: 1px solid #e41050;
+    top:-5px;
+    left: -6px;
+    width: calc(100% - 10px);
+    height: calc(100% - 10px);
+    border-radius:22px;
+ }
+`}
+`
