be8af1dcbfa446a8e9885192baa9112f4b6109e5
Bruno Paulon <paulonbruno9@gmail.com>
2022-12-14 15:10:26 +0000

    update

--- blackbird/src/components/header/styles.js
+++ blackbird/src/components/header/styles.js
@@ -6,7 +6,7 @@ max-width: 80%;
 height:47px;
 
 display:flex;
-justify-content:center;
+justify-content:space-between;
 align-items:center;
 margin:0 auto;
 
@@ -19,9 +19,27 @@ align-items:center;
 
 `
 
-export const Column = styled.div `
+export const Column = styled.div
+
+`
 display:flex;
 flex-direction:row;
 align-items:center;
 
+`
+export const Wrapper = styled.div `
+background-color:#151515;
+width:100%;
+height:47px;
+display:flex;
+justify-content:center;
+align-items:center;
+`
+export const BuscarInputContainer = styled.div `
+width:175px;
+height:30px;
+background-color:#2d2d37;
+border-radius:8px;
+padding: 2px 5px;
+margin 0 12px;
 `
---
+++ blackbird/src/pages/home/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  blackground-color:purple;
+  color:violet;
+  font-size:24px;
+  font-weight:bold;
+
+  `
